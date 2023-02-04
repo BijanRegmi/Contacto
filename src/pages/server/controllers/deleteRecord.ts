fef33a21d4e9cb66ea6561e995a0475ece570af2
(void *)0x0 <55084653+BijanRegmi@users.noreply.github.com>
2023-02-04 14:29:05 +0545

    delete proc

---
+++ src/pages/server/controllers/deleteRecord.ts
@@ -0,0 +1,27 @@
+import { TypeOf, z } from "zod"
+import { Context } from "@/server/context"
+import { TRPCError } from "@trpc/server"
+
+export const deleteRecordSchema = z.object({ id: z.string() })
+
+export const deleteRecordProc = async ({
+	input,
+	ctx,
+}: {
+	input: TypeOf<typeof deleteRecordSchema>
+	ctx: Context
+}) => {
+	const { db } = ctx
+
+	const queryStr =
+		'DELETE FROM record WHERE id=$1 AND "accountId"=$2 RETURNING id'
+	const response = await db.query(queryStr, [input.id, ctx.userId])
+
+	if (response.rows.length == 0)
+		throw new TRPCError({
+			code: "BAD_REQUEST",
+			message: "Deleting record failed.",
+		})
+
+	return { success: true, id: response.rows[0].id }
+}
--- src/pages/server/routers/records.ts
+++ src/pages/server/routers/records.ts
@@ -1,6 +1,13 @@
 import { privateProcedure, router } from "@/server/trpc"
-import { addRecordProc, addRecordSchema } from "../controllers/addRecord"
+import { addRecordProc, addRecordSchema } from "@/server/controllers/addRecord"
+import {
+	deleteRecordProc,
+	deleteRecordSchema,
+} from "../controllers/deleteRecord"
 
 export const recordsRouter = router({
 	add: privateProcedure.input(addRecordSchema).mutation(addRecordProc),
+	delete: privateProcedure
+		.input(deleteRecordSchema)
+		.mutation(deleteRecordProc),
 })
