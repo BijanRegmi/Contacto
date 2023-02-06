e1e21725f6bfedbe9cd202ea88642cab814ef313
(void *)0x0 <55084653+BijanRegmi@users.noreply.github.com>
2023-02-06 20:32:42 +0545

    .

--- src/server/schema.sql
+++ src/server/schema.sql
@@ -1,38 +1,33 @@
 -- Drop previous db if exists
-DROP DATABASE phonebook;
+DROP DATABASE contacto;
 -- CreateDatabase
-CREATE DATABASE phonebook;
-\c phonebook;
+CREATE DATABASE contacto;
+\c contacto;
 
 -- Enable uuid extension
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 -- Create account table
-CREATE TABLE "account" (
-    "id" uuid DEFAULT uuid_generate_v4 (),
-    "email" TEXT NOT NULL,
-    "password" TEXT NOT NULL,
-		"username" TEXT NOT NULL,
+CREATE TABLE account (
+    id uuid DEFAULT uuid_generate_v4 (),
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+		username TEXT NOT NULL,
 
-    CONSTRAINT "account_pkey" PRIMARY KEY ("id")
+    CONSTRAINT account_pkey PRIMARY KEY (id)
 );
 
 -- Create record table
-CREATE TABLE "record" (
-    "id" uuid DEFAULT uuid_generate_v4 (),
-    "firstname" TEXT NOT NULL,
-    "lastname" TEXT,
-    "company" TEXT,
-    "phone" BIGINT NOT NULL,
-    "email" TEXT,
-    "accountId" uuid,
-		"image" TEXT,
+CREATE TABLE record (
+    id uuid DEFAULT uuid_generate_v4 (),
+    firstname TEXT NOT NULL,
+    lastname TEXT,
+    company TEXT,
+    phone BIGINT NOT NULL,
+    email TEXT UNIQUE,
+    accountId uuid,
+		image TEXT,
 
-    CONSTRAINT "record_pkey" PRIMARY KEY ("id")
+    CONSTRAINT record_pkey PRIMARY KEY (id),
+		CONSTRAINT record_accountId_fkey FOREIGN KEY (accountId) REFERENCES account(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
-
-CREATE UNIQUE INDEX "account_id_key" ON "account"("id");
-CREATE UNIQUE INDEX "account_email_key" ON "account"("email");
-CREATE UNIQUE INDEX "record_id_key" ON "record"("id");
-
-ALTER TABLE "record" ADD CONSTRAINT "record_accountId_fkey" FOREIGN KEY ("accountId") REFERENCES "account"("id") ON DELETE CASCADE ON UPDATE CASCADE;
